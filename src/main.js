09a476c6705a5b0ca1ca020a105fc71f7b714050
Andrew Nekowitsch <anekowitsch@gmail.com>
2019-12-01 16:34:11 -0600

    sushiburrito

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+// Andrew
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+Vue.use(VueMaterial)
+import 'vue-material/dist/theme/black-green-light.css'
+// Andrew
 
+// more grace import third package !
+import moment from 'moment'
+import axios from 'axios'
+import curvejs from 'curvejs'
+Object.defineProperty(Vue.prototype, '$moment', { value: moment });
+Object.defineProperty(Vue.prototype, '$axios', { value: axios });
+Object.defineProperty(Vue.prototype, '$curvejs', { value: curvejs });
+
+// eslint-disable-next-line no-new
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    el: '#app',
+    render: h => h(App)
+});
+
