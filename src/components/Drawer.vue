d494ff08f0ee06bcaed950bc394f99b7eb9776f4
Andrew Nekowitsch <anekowitsch@gmail.com>
2020-03-15 19:43:59 -0500

    updating cards

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,106 +1,107 @@
 <template>
-  <md-app>
-    <md-app-toolbar>
-      <NavBar v-bind:menuVisible="this.visible" v-on:toggleDrawer="toggleMenu" v-on:recipeString="getRecipe" />
-    </md-app-toolbar>
+	<md-app>
+		<md-app-toolbar>
+			<NavBar v-bind:menuVisible="this.visible" v-on:toggleDrawer="toggleMenu" v-on:recipeString="getRecipe" />
+		</md-app-toolbar>
 
-    <md-app-drawer v-on:toggleDrawer="toggleMenu" :md-active.sync="visible">
-      <filters v-bind:param="this.parameters" v-on:emitParam="getParams" />
-    </md-app-drawer>
-    <md-app-content id="cont">
-		<div id="recipecards" v-for="rec in recipeJSON" v-bind:key="rec.calories">
-          <RecipeCard
-            class="recipes"
-            :title="rec.recipe.label"
-            :dietLabels="rec.dietlabels"
-            :instructions="rec.ingredientLines"
-            :image="rec.recipe.image"
-          ></RecipeCard>
-		   <div id="balls"></div>
-		   <div id="overlay">
-          <div id="overlay_white" class="overlay bc-white">
-            <div id="overlay_exit" @click="toggleOverlay" class="overlay">X</div>
-            <div>
-              <h1 id="overlay_title" class="overlay">This is a Title</h1>
-              <br />
-              <br />
-              <br />
-              <div id="youtube">
-                <iframe
-                  src="https://www.youtube.com/embed/tgbNymZ7vqY"
-                  width="560"
-                  height="315"
-                  frameborder="0"
-                  allowfullscreen
-                ></iframe>
-              </div>
-              <div>
-                <h2 id="overlay_instructions"></h2>
-              </div>
-            </div>
-          </div>
-        </div>
-        </div>
-    </md-app-content>
-  </md-app>
+		<md-app-drawer v-on:toggleDrawer="toggleMenu" :md-active.sync="visible">
+			<filters v-bind:param="this.parameters" v-on:paramEmitted="getParams" />
+		</md-app-drawer>
+		<md-app-content id="cont">
+			<div id="recipecards" v-for="rec in recipeJSON" v-bind:key="rec.calories">
+				<RecipeCard2
+					class="recipes"
+					:title="rec.recipe.label"
+					:dietLabels="rec.dietlabels"
+					:instructions="rec.ingredientLines"
+					:image="rec.recipe.image"
+				/>
+				<div id="balls"></div>
+				<div id="overlay">
+					<div id="overlay_white" class="overlay bc-white">
+						<div id="overlay_exit" @click="toggleOverlay" class="overlay">X</div>
+						<div>
+							<h1 id="overlay_title" class="overlay">This is a Title</h1>
+							<br />
+							<br />
+							<br />
+							<div id="youtube">
+								<iframe
+									src="https://www.youtube.com/embed/tgbNymZ7vqY"
+									width="560"
+									height="315"
+									frameborder="0"
+									allowfullscreen
+								></iframe>
+							</div>
+							<div>
+								<h2 id="overlay_instructions"></h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</md-app-content>
+	</md-app>
 </template>
 
 <script>
-import Filters from "./Filters.vue";
-import NavBar from "./NavBar.vue";
-import RecipeCard from "./RecipeCard.vue";
+import Filters from './Filters.vue';
+import NavBar from './NavBar.vue';
+import RecipeCard2 from './RecipeCard2.vue';
 export default {
-  components: {
-	  RecipeCard,
-    Filters,
-    NavBar,
-  },
-  methods: {
-    toggleMenu(value) {
-      this.visible = value;
+	components: {
+		RecipeCard2,
+		Filters,
+		NavBar,
 	},
-	getParams(value) {
-		this.parameters = value;
+	methods: {
+		toggleMenu(value) {
+			this.visible = value;
+		},
+		getParams(value) {
+			this.parameters = value;
+			console.log('params: ' + value);
+		},
+		getRecipe: async function(value) {
+			console.log('getRecipe()');
+			this.query = value;
+			//bounce off of api
+			var url = `https://culinary-companion-api.herokuapp.com/recipes/?search=${String(
+				//var url = `http://localhost:8080/recipes/?search=${String(
+				this.query
+			)}${String(this.parameters)}`;
+			url.replace(`%20`, '');
+			var response = await fetch(url).then(resp => resp.json());
+			this.recipeJSON = [];
+			for (var index in response) {
+				this.recipeJSON.push(response[index]);
+			}
+			console.log('Recipes:');
+			console.log(this.recipeJSON);
+		},
 	},
-	getRecipe: async function(value) {
-		console.log("getRecipe()");
-		this.query = value;
-		//bounce off of api
-		 var url = `https://culinary-companion-api.herokuapp.com/recipes/?search=${String(
-            //var url = `http://localhost:8080/recipes/?search=${String(
-            this.query
-          )}${String(this.parameters)}`;
-          url.replace("%20", "");
-          var response = await fetch(url).then(resp => resp.json());
-          this.recipeJSON = [];
-          for (var index in response) {
-            this.recipeJSON.push(response[index]);
-          }
-          console.log("Recipes:");
-          console.log(this.recipeJSON);
-	}
-  },
-  data: () => ({
-	visible: false,
-	parameters: "",
-	query: "",
-	recipeJSON: []
-  })
+	data: () => ({
+		visible: false,
+		parameters: '',
+		query: '',
+		recipeJSON: [],
+	}),
 };
 </script>
 
 <style scoped>
 .md-drawer {
-  width: 230px;
-  max-width: calc(100vw - 125px);
+	width: 230px;
+	max-width: calc(100vw - 125px);
 }
 .nav-title {
-  color: black !important;
-  font-size: 18px;
-  line-height: 26px;
+	color: black !important;
+	font-size: 18px;
+	line-height: 26px;
 }
 .page-container {
-  display: flex;
-  flex-direction: column;
+	display: flex;
+	flex-direction: column;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,35 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
 // Andrew
-import VueMaterial from 'vue-material'
-import 'vue-material/dist/vue-material.min.css'
-Vue.use(VueMaterial)
-import 'vue-material/dist/theme/black-green-light.css'
-import axios from 'axios'
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+Vue.use(VueMaterial);
+import 'vue-material/dist/theme/black-green-light.css';
+import axios from 'axios';
+import router from './router';
 Object.defineProperty(Vue.prototype, '$axios', { value: axios });
 
 // eslint-disable-next-line no-new
 new Vue({
-    el: '#app',
-    render: h => h(App)
+	el: '#app',
+	router,
+	render: h => h(App),
 });
-Vue.component('async', function (resolve, reject) {
-	setTimeout(function () {
-	  // Pass the component definition to the resolve callback
-	  resolve({
-		template: '<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">'
-	  })
-	}, 1000)
-  })
+Vue.component('async', function(resolve, reject) {
+	setTimeout(function() {
+		// Pass the component definition to the resolve callback
+		resolve({
+			template:
+				'<link rel="stylesheet" href="//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Material+Icons">',
+		});
+	}, 1000);
+});
 
+Vue.use(Auth0Plugin, {
+	domain,
+	clientId,
+	onRedirectCallback: appState => {
+		router.push(appState && appState.targetUrl ? appState.targetUrl : window.location.pathname);
+	},
+});
