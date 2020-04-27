f6509b468d11186a8290cb28bf518f3dfe85d7f3
MarkSelyukov <33294280+MarkSelyukov@users.noreply.github.com>
2020-04-26 23:05:52 -0500

    This is for Pat

---
+++ src/views/TechniquePage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+       {{this.$route.params.id}}
+  </div>
+</template>
+
+<script>
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+
+export default {
+     name: "recipePage",
+  components: {
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  font-size: 100%;
+  padding: 0;
+  margin: 0;
+  background: white;
+}
+h4 {
+  font-size: 200%;
+  text-align: center;
+}
+.basic {
+  font-size: 100%;
+  padding: 0;
+  margin: 0;
+  background: white;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 500px;
+}
+h1 {
+  text-align: center;
+}
+</style>
